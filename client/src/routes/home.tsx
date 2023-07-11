258fab21733b630fbced9153c3955d46913468d6
RedCrafter07 <gh@r07.dev>
2023-07-11 19:23:33 +0200

    Add banner to homepage

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,15 +1,16 @@
+import Banner from '../components/Banner';
+
 export default function Home() {
 	return (
-		<div className='bg-[#111111] text-white min-h-screen p-2'>
-			<div className='h-16 w-full bg-[#222] fixed top-0 left-0 p-2 flex'>
+		<div className='bg-[#111111] text-white min-h-screen overflow-x-hidden'>
+			<div className='h-16 w-full bg-[#222] fixed top-0 left-0 p-2 flex z-50'>
 				<p className='uppercase text-2xl my-auto font-black tracking-widest'>
 					Skytransfer
 				</p>
 			</div>
 			<div className='h-16' />
-			<div className='mx-auto container'>
-				<h1>Welcome to Sky Transfer!</h1>
-			</div>
+			<Banner />
+			<div className='mx-auto container p-2'></div>
 		</div>
 	);
 }
