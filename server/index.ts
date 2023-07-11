4645f13d7fe772a18c0153554d5a321835dd1c21
RedCrafter07 <gh@r07.dev>
2023-07-11 18:55:29 +0200

    Remove stuff

--- server/index.ts
+++ server/index.ts
@@ -1,48 +1,13 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import chalk from 'chalk';
-import mongoose from 'mongoose';
 const app = express();
-import { version } from './package.json';
-import { access, readFile } from 'fs/promises';
-import { constants } from 'fs';
-
-// DATABASE
-const MongoDBConfig: {
-	ip: string;
-	port: string;
-	db: string;
-	username: string;
-	password: string;
-} = {
-	db: 'main',
-	ip: '0.0.0.0',
-	port: '27017',
-	password: 'passwd',
-	username: 'user',
-};
-
-mongoose
-	.connect(
-		`mongodb://${MongoDBConfig.username}:${MongoDBConfig.password}@${MongoDBConfig.ip}:${MongoDBConfig.port}`,
-		{
-			dbName: MongoDBConfig.db,
-		},
-	)
-	.then(() => {
-		console.log(chalk.blue('[DATABASE]: Connected!'));
-	});
 
 // FILE SERVING
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
 
 // API
-
-app.get('/api/version', (req, res) => {
-	if (req.query.json !== undefined) res.json(version);
-	else res.send(version);
-});
 
 // Routes
 app.get('/', sendFile);
