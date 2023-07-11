ca9bf848446920d7e295a85224cebaaeb0bbef82
RedCrafter07 <gh@r07.dev>
2023-07-11 19:23:26 +0200

    Add medium font

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import App from './App';
 import './index.css';
 import '@fontsource/figtree';
 import '@fontsource/figtree/900.css';
+import '@fontsource/figtree/700.css';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
