2ee1fe19fb9b5f44bc305f53828f8eb6bd16ad38
RedCrafter07 <gh@r07.dev>
2023-07-11 18:55:57 +0200

    Import figtree

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import '@fontsource/figtree';
+import '@fontsource/figtree/900.css';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
