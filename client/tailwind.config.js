e1a4a318062841febd6734ab9808dfac03964ae4
RedCrafter07 <gh@r07.dev>
2023-07-11 21:43:03 +0200

    Add glow styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.tsx'],
 	theme: {
-		extend: {},
+		extend: {
+			dropShadow: {
+				glow: '0 0px 20px #ccccffaa',
+			},
+		},
 	},
 	plugins: [],
 };
