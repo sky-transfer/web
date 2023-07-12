7e21de0c917e7cc31ab45932b7c449ed32d9f8d2
RedCrafter07 <gh@r07.dev>
2023-07-12 14:16:41 +0200

    First footer draft

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,3 +1,19 @@
 export default function Footer() {
-	return <></>;
+	return (
+		<div className='grid grid-cols-1 lg:grid-cols-3 gap-4 p-2 bg-[#111] text-center lg:text-left'>
+			<div className='my-auto'>
+				<h1 className='text-3xl font-black uppercase tracking-widest text-[#ccf]'>
+					Skytransfer
+				</h1>
+				<p>©️ 2023 RedCrafter07</p>
+			</div>
+			<div className='my-auto'>
+				<p className='text-[#ccf] font-bold text-xl'>Important links</p>
+				<div className='my-2' />
+				<a className='underline text-white' href='/'>
+					Home
+				</a>
+			</div>
+		</div>
+	);
 }
