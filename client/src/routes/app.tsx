99cb607627ad2cf0ccad5df879a4ca340e9b8431
RedCrafter07 <gh@r07.dev>
2023-07-12 21:09:49 +0200

    Add some more stuff to the app

--- client/src/routes/app.tsx
+++ client/src/routes/app.tsx
@@ -34,13 +34,45 @@ export default function App() {
 	return (
 		<div>
 			<div className='flex min-h-screen'>
-				<div className='flex flex-row w-full my-auto'>
-					<div className='flex-1'>
-						<h1 className='text-[#ccf] text-3xl font-bold'>Send text</h1>
-						<p className='opacity-80'>Scan the QR Code</p>
-						<QRCodeCanvas value={code} />
+				<div className='container mx-auto p-2'>
+					<div className='flex flex-col gap-8 lg:flex-row w-full h-full items-center'>
+						<div className='flex-1 flex flex-col items-center justify-center'>
+							<h1 className='text-[#ccf] text-3xl font-bold'>Send text</h1>
+							<div className='my-8' />
+							<h3 className='opacity-80 text-xl'>Scan the QR Code</h3>
+							<div className='my-4' />
+							<QRCodeCanvas value={code} bgColor='#0000' fgColor='#ccf' />
+							<div className='my-8' />
+							<h3 className='opacity-80 text-xl'>
+								or type in the code manually:
+							</h3>
+							<div className='my-4' />
+							<p>{code}</p>
+						</div>
+						<div className='flex flex-row lg:flex-col h-full items-center gap-4'>
+							<div className='h-full w-px bg-white opacity-10 flex-1' />
+							<p>OR</p>
+							<div className='h-full w-px bg-white opacity-10 flex-1' />
+						</div>
+						<div className='flex-1'>
+							<h1 className='text-[#ccf] text-3xl font-bold'>Receive</h1>
+							<div className='my-8' />
+							<p>Type in code</p>
+							<div className='my-4' />
+							<form
+								onSubmit={(e) => {
+									e.preventDefault();
+								}}
+							>
+								<input
+									name='code'
+									type='text'
+									className='w-full p-2 focus:outline-none border-2 border-white border-opacity-10 placeholder:text-white focus:border-opacity-30 placeholder:opacity-50 bg-[#222] rounded-lg transition-all duration-100'
+									placeholder='Code'
+								/>
+							</form>
+						</div>
 					</div>
-					<div className='flex-1'></div>
 				</div>
 			</div>
 			<Footer />
