9eb44abd386bb7aaa2731c5b4e0ba6fa167b8371
RedCrafter07 <gh@r07.dev>
2023-07-12 13:25:19 +0200

    Add useScroll function

---
+++ client/src/util/useScroll.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+function useScroll(ref: React.MutableRefObject<HTMLDivElement>) {
+	const [scrollY, setScrollY] = useState(0);
+	const [scrollX, setScrollX] = useState(0);
+
+	const component = ref.current || document.body;
+
+	useEffect(() => {
+		component.addEventListener('scroll', onScroll);
+
+		function onScroll() {
+			setScrollY(component.scrollTop);
+			setScrollX(component.scrollLeft);
+		}
+
+		return () => {
+			component.removeEventListener('scroll', onScroll);
+		};
+	}, []);
+
+	return { scrollY, scrollX };
+}
