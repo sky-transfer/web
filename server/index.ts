13366de6d71ce4658a6b5a246448b48d540f5980
RedCrafter07 <gh@r07.dev>
2023-07-12 19:46:25 +0200

    Add socket.io server

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,56 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import chalk from 'chalk';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
 const app = express();
+const server = createServer(app);
+
+const socket = new Server(server, {
+	transports: ['websocket'],
+});
+
+socket.on('connect', (socket) => {
+	socket.emit('deviceID', genDeviceID());
+
+	const code = genCode();
+	socket.emit('code', code);
+	socket.join(code);
+	socket.data.type = 'sender';
+
+	socket.on('code', async (c: string) => {
+		socket.data.type = 'receiver';
+		socket.data.code = c;
+
+		const allSockets = await socket.to(c).fetchSockets();
+		if (allSockets.length <= 1) {
+			socket.emit('error', 'No sender found');
+			return;
+		}
+
+		socket.join(c);
+	});
+
+	socket.on('disconnect', () => {
+		socket.leave(socket.data.code);
+
+		// disconnect other socket
+		socket.to(socket.data.code).emit('disconnect');
+	});
+
+	socket.on('text', async (data: string) => {
+		const members = await socket.to(socket.data.code).fetchSockets();
+
+		console.log(members);
+
+		members
+			.filter((s) => s.data.type == 'receiver')
+			.forEach((s) => {
+				s.emit('text', data);
+			});
+	});
+});
 
 // FILE SERVING
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
@@ -19,7 +68,7 @@ app.get('/download/android', (req, res) => {
 
 app.get('*', notFound);
 
-app.listen(3000, () => {
+server.listen(3000, () => {
 	console.log(chalk.green(`[SERVER]: Listening on port 3000`));
 });
 
@@ -29,6 +78,15 @@ function sendFile(req: Request, res: Response) {
 
 function notFound(req: Request, res: Response) {
 	res.status(404).sendFile(path.join(__dirname, '../client/build/index.html'));
+}
+
+function genDeviceID() {
+	let code = '';
+
+	// include letters and numbers
+	for (let i = 0; i < 16; i++) {
+		code += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+	}
 }
 
 function genCode() {
