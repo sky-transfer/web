82dc8141538c1d746972e0df187a0039783ca20a
RedCrafter07 <gh@r07.dev>
2023-07-26 18:50:14 +0200

    Add circles to separate div

--- client/src/components/Banner.tsx
+++ client/src/components/Banner.tsx
@@ -1,5 +1,6 @@
 import { IconRocket } from '@tabler/icons-react';
 import { useNavigate } from 'react-router-dom';
+import useCursor from '../util/useCursor';
 
 export default function Banner(props: { scrollY: number }) {
 	const { scrollY } = props;
@@ -8,11 +9,13 @@ export default function Banner(props: { scrollY: number }) {
 
 	return (
 		<div className='bg-[#111111] w-full h-screen relative overflow-hidden'>
-			<div className='absolute top-0 left-0 h-64 lg:h-[40rem] aspect-square rounded-full bg-[#ccf] -translate-x-1/2 -translate-y-1/2 blur-3xl lg:blur-[192px]' />
+			<div className='w-full h-full absolute top-0 left-0'>
+				<div className='absolute top-0 left-0 h-64 lg:h-[40rem] aspect-square rounded-full bg-[#ccf] -translate-x-1/2 -translate-y-1/2 blur-3xl lg:blur-[192px]' />
+				<div className='absolute right-4 lg:right-10 bottom-4 h-60 md:h-72 aspect-square rounded-full bg-[#3333CD] blur-3xl lg:blur-[96px]' />
+				<div className='absolute right-8 lg:right-40 top-20 h-56 md:h-60 aspect-square rounded-full bg-[#5A5AD6] blur-3xl lg:blur-[96px]' />
+				<div className='absolute left-10 lg:left-80 bottom-24 h-48 md:h-80 aspect-square rounded-full bg-[#9090EB] blur-3xl lg:blur-[128px]' />
+			</div>
 
-			<div className='absolute right-4 lg:right-10 bottom-4 h-60 md:h-72 aspect-square rounded-full bg-[#3333CD] blur-3xl lg:blur-[96px]' />
-			<div className='absolute right-8 lg:right-40 top-20 h-56 md:h-60 aspect-square rounded-full bg-[#5A5AD6] blur-3xl lg:blur-[96px]' />
-			<div className='absolute left-10 lg:left-80 bottom-24 h-48 md:h-80 aspect-square rounded-full bg-[#9090EB] blur-3xl lg:blur-[128px]' />
 			<div className='absolute top-0 left-0 w-full h-full grid place-items-center'>
 				<div
 					className='text-center'
