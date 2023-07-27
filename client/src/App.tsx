c7549c3195c423591974a93b16657cb55e90fb9c
RedCrafter07 <gh@r07.dev>
2023-07-27 21:21:46 +0200

    Add better fallback

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,7 @@ import Wrappers from './wrappers';
 export default function App() {
 	return (
 		<>
-			<Suspense
-				fallback={
-					<div className='grid place-items-center'>
-						<h1 className='text-3xl'>Loading...</h1>
-					</div>
-				}
-			>
-				{/* You can replace "BrowserRouter with other react-router routers." */}
+			<Suspense fallback={<div className='h-screen w-full bg-[#111]' />}>
 				<BrowserRouter>
 					<Wrappers />
 				</BrowserRouter>
