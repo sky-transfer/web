f362fe5ed57bef870d1a99d34b31b313b5cf412e
RedCrafter07 <gh@r07.dev>
2023-07-27 21:33:19 +0200

    Stagger blobs

--- client/src/components/Banner.tsx
+++ client/src/components/Banner.tsx
@@ -1,7 +1,23 @@
 import { IconRocket } from '@tabler/icons-react';
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence, TargetAndTransition, motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 import useCursor from '../util/useCursor';
+
+const container: Record<string, TargetAndTransition> = {
+	hidden: { opacity: 0 },
+	show: {
+		opacity: 1,
+		transition: {
+			staggerChildren: 0.05,
+			delayChildren: 0.3,
+		},
+	},
+};
+
+const item = {
+	hidden: { opacity: 0 },
+	show: { opacity: 1 },
+};
 
 export default function Banner(props: { scrollY: number }) {
 	const { scrollY } = props;
@@ -11,19 +27,36 @@ export default function Banner(props: { scrollY: number }) {
 
 	return (
 		<div className='bg-[#111111] w-full h-screen relative overflow-hidden'>
-			<div
-				className='w-full h-full absolute top-0 left-0'
-				style={{
-					transform: `translate(${x / 10}px, ${y / 10}px)`,
-					// smooth the translation
-					transition: 'transform 0.5s cubic-bezier(.22,.42,.31,.99)',
-				}}
-			>
-				<div className='absolute top-0 left-0 h-64 lg:h-[40rem] aspect-square rounded-full bg-[#ccf] -translate-x-1/2 -translate-y-1/2 blur-3xl lg:blur-[192px]' />
-				<div className='absolute right-4 lg:right-10 bottom-4 h-60 md:h-72 aspect-square rounded-full bg-[#3333CD] blur-3xl lg:blur-[96px]' />
-				<div className='absolute right-8 lg:right-40 top-20 h-56 md:h-60 aspect-square rounded-full bg-[#5A5AD6] blur-3xl lg:blur-[96px]' />
-				<div className='absolute left-10 lg:left-80 bottom-24 h-48 md:h-80 aspect-square rounded-full bg-[#9090EB] blur-3xl lg:blur-[128px]' />
-			</div>
+			<AnimatePresence>
+				<motion.div
+					variants={container}
+					initial='hidden'
+					animate='show'
+					className='w-full h-full absolute top-0 left-0'
+					style={{
+						transform: `translate(${x / 10}px, ${y / 10}px)`,
+						// smooth the translation
+						transition: 'transform 0.5s cubic-bezier(.22,.42,.31,.99)',
+					}}
+				>
+					<motion.div
+						variants={item}
+						className='absolute top-0 left-0 h-64 lg:h-[40rem] aspect-square rounded-full bg-[#ccf] -translate-x-1/2 -translate-y-1/2 blur-3xl lg:blur-[192px]'
+					/>
+					<motion.div
+						variants={item}
+						className='absolute right-4 lg:right-10 bottom-4 h-60 md:h-72 aspect-square rounded-full bg-[#3333CD] blur-3xl lg:blur-[96px]'
+					/>
+					<motion.div
+						variants={item}
+						className='absolute right-8 lg:right-40 top-20 h-56 md:h-60 aspect-square rounded-full bg-[#5A5AD6] blur-3xl lg:blur-[96px]'
+					/>
+					<motion.div
+						variants={item}
+						className='absolute left-10 lg:left-80 bottom-24 h-48 md:h-80 aspect-square rounded-full bg-[#9090EB] blur-3xl lg:blur-[128px]'
+					/>
+				</motion.div>
+			</AnimatePresence>
 
 			<div className='absolute top-0 left-0 w-full h-full grid place-items-center'>
 				<div
