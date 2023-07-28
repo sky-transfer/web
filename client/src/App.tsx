9fc754420b9760f535f8adf5368ec0970cd253f4
RedCrafter07 <gh@r07.dev>
2023-07-28 11:49:56 +0200

    Wait for fonts to load

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,63 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Wrappers from './wrappers';
+import LogoText from './components/LogoText';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function App() {
+	const [loading, setLoading] = useState(true);
+	const [hideBar, setHideBar] = useState(false);
+
+	useEffect(() => {
+		// wait for the font to load
+		document.fonts.ready.then(() => {
+			setLoading(false);
+			setTimeout(() => {
+				setHideBar(true);
+			}, 750);
+		});
+	}, []);
+
+	const LoadingComponent = () => {
+		return (
+			<div className='h-screen w-full bg-[#111] grid place-items-center'>
+				<div className='w-1/4 h-auto'>
+					<LogoText />
+				</div>
+			</div>
+		);
+	};
+
 	return (
 		<>
-			<Suspense fallback={<div className='h-screen w-full bg-[#111]' />}>
-				<BrowserRouter>
-					<Wrappers />
-				</BrowserRouter>
-			</Suspense>
+			<div
+				className={`absolute top-0 left-0 h-1 bg-[#ccf] ${
+					loading ? 'w-0' : 'w-full'
+				} ${
+					hideBar ? 'opacity-0' : ''
+				} z-[500] transition-all duration-500 ease-in-out`}
+			/>
+			<AnimatePresence mode='wait' initial={false}>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{
+						duration: 0.3,
+					}}
+					key={`${loading ? 'loading' : 'not loading'}`}
+				>
+					{loading ? (
+						<LoadingComponent />
+					) : (
+						<Suspense fallback={<div className='h-screen w-full bg-[#111]' />}>
+							<BrowserRouter>
+								<Wrappers />
+							</BrowserRouter>
+						</Suspense>
+					)}
+				</motion.div>
+			</AnimatePresence>
 		</>
 	);
 }
