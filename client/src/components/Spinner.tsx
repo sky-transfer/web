225d3c2c16b71768d53558654510bf8d24fb5478
RedCrafter07 <gh@r07.dev>
2023-07-28 22:04:53 +0200

    Add spinner component

---
+++ client/src/components/Spinner.tsx
@@ -0,0 +1,10 @@
+export default function Spinner() {
+	return (
+		<div
+			className='border-8 rounded-full w-16 aspect-square animate-spin'
+			style={{
+				borderColor: '#ccf transparent transparent',
+			}}
+		></div>
+	);
+}
