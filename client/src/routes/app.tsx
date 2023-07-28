fa5ca6a04019f4c5e53353f5ea1460a6b6e7efc9
RedCrafter07 <gh@r07.dev>
2023-07-28 15:34:32 +0200

    Use URL as default value

--- client/src/routes/app.tsx
+++ client/src/routes/app.tsx
@@ -103,6 +103,7 @@ export default function App() {
 										className='w-full p-2 focus:outline-none border-2 border-white border-opacity-10 placeholder:text-white focus:border-opacity-30 placeholder:opacity-50 bg-[#111] rounded-l-lg transition-all duration-100'
 										placeholder='https://sky-transfer.redcrafter07.de'
 										required
+										defaultValue={url}
 									/>
 
 									<button className='bg-green-600 hover:bg-opacity-75 active:scale-95 py-2 px-4 text-white rounded-r-lg'>
