34abe9c1aabca41f21073c797611006ea554ba69
RedCrafter07 <gh@r07.dev>
2023-07-29 20:25:08 +0200

    Add scroll effect

--- client/src/routes/selfHost.tsx
+++ client/src/routes/selfHost.tsx
@@ -1,7 +1,30 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import { textContainer, textItem } from '../components/Banner';
+import { useEffect, useRef, useState } from 'react';
 
 export default function selfHost() {
+	const [navDetach, setNavDetach] = useState(false);
+	const [scrollY, setScrollY] = useState(0);
+	const mainComponent = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (!mainComponent.current) return;
+		mainComponent.current.addEventListener('scroll', onScroll);
+
+		function onScroll() {
+			setScrollY(mainComponent.current!.scrollTop);
+			if (mainComponent.current!.scrollTop > window.innerHeight / 2) {
+				setNavDetach(true);
+			} else {
+				setNavDetach(false);
+			}
+		}
+
+		return () => {
+			mainComponent.current?.removeEventListener('scroll', onScroll);
+		};
+	}, [mainComponent.current]);
+
 	return (
 		<div className='bg-[#111111] text-white min-h-screen overflow-x-hidden max-h-screen w-full overflow-y-visible'>
 			<div className='h-screen'>
@@ -12,7 +35,16 @@ export default function selfHost() {
 						animate='show'
 						className='grid place-items-center h-full text-center'
 					>
-						<div className='flex flex-col gap-4'>
+						<div
+							className='flex flex-col gap-4'
+							style={{
+								transform: `translateY(${
+									scrollY < window.innerHeight
+										? scrollY / 3
+										: window.innerHeight / 3
+								}px)`,
+							}}
+						>
 							<motion.h1
 								variants={textItem}
 								className='text-3xl md:text-4xl lg:text-5xl font-bold tracking-tight'
